02cde551db602c62ead11fef7a29095e23ff93c8
zelca <zelcadev@gmail.com>
2016-03-22 22:54:42 +0100

    cracking eggs added

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Feel free to share your feedback.
 <p><a href="https://www.codeeval.com/open_challenges/155/">ASCII decryption</a></p>
 <p><a href="https://www.codeeval.com/open_challenges/109/">Bay bridges</a></p>
 <p><a href="https://www.codeeval.com/open_challenges/239/">As quick as a flash</a></p>
+<p><a href="https://www.codeeval.com/open_challenges/151/">Cracking eggs</a></p>
---
+++ src/CrackingEggs.scala
@@ -0,0 +1,29 @@
+/**
+  * See <a href="https://www.codeeval.com/open_challenges/151/">Cracking eggs</a>
+  */
+object CrackingEggs extends Challenge {
+
+  val lines = scala.io.Source.fromFile(args(0)).getLines()
+
+  lines.collect {
+    case Input(tries, floors) =>
+      (1 to floors).find(i => floor(tries, i) >= floors).get
+  } foreach println
+
+  def floor(e: Int, t: Int): Int = (e, t) match {
+    case (1, n) => n
+    case (x, 0) => 0
+    case (x, n) => 1 + floor(x - 1, n - 1) + floor(x, n - 1)
+  }
+
+  object Input {
+
+    // 2 100
+    def unapply(line: String) = line.split(" ").toList match {
+      case tries :: floors :: Nil => Some(tries.toInt, floors.toInt)
+      case _ => None
+    }
+
+  }
+
+}
