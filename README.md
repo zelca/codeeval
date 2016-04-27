3ae669cbe8c673590bf1b5507a680134815bd999
zelca <zelcadev@gmail.com>
2016-04-27 23:33:49 +0200

    solution for Palindromic Ranges added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Solutions for CodeEval challenges in Scala. The functional way is applied whenev
 
 Any feedback is appreciated.
 
-## Solved challenges [39 of 77]:
+## Solved challenges [40 of 77]:
 
  * [Alphabet Blocks](https://www.codeeval.com/open_challenges/201/)
  * [Climbing Stairs](https://www.codeeval.com/open_challenges/64/)
@@ -45,3 +45,4 @@ Any feedback is appreciated.
  * [Telephone Words](https://www.codeeval.com/open_challenges/59/)
  * [Levenshtein Distance](https://www.codeeval.com/open_challenges/58/)
  * [Distinct Triangles](https://www.codeeval.com/open_challenges/188/)
+ * [Palindromic Ranges](https://www.codeeval.com/open_challenges/47/)
---
+++ src/PalindromicRanges.scala
@@ -0,0 +1,35 @@
+/**
+  * See <a href="https://www.codeeval.com/open_challenges/47/">Palindromic Ranges</a>
+  */
+object PalindromicRanges extends Challenge {
+
+  val lines = scala.io.Source.fromFile(args(0)).getLines().filter(_.length > 0)
+
+  lines.collect {
+    case Input(range) =>
+      val palindromes = range.map(x => fp(x.toString.toList)).toList
+      eval(palindromes)
+  } foreach println
+
+  def fp(x: List[Char]): Int =
+    if (x.size <= 1) 1 else if (x.head == x.last) fp(x.tail.init) else 0
+
+  def eval(i: List[Int]): Int = i match {
+    case Nil => 0
+    case x :: xs =>
+      i.foldLeft((0, 0)) {
+        (a, x) => ((a._1 + x) % 2, a._2 + 1 - (a._1 + x) % 2)
+      }._2 + eval(xs)
+  }
+
+  object Input {
+
+    // 1 2
+    def unapply(line: String) = line.split(" ").toList match {
+      case left :: right :: Nil => Some(left.toInt to right.toInt)
+      case _ => None
+    }
+
+  }
+
+}
