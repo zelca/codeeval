797a2696369032ac9ae494a2235b1601a1198c4b
zelca <zelcadev@gmail.com>
2016-06-10 00:03:10 +0200

    add time and memory statistics to Challenge

--- src/Challenge.scala
+++ src/Challenge.scala
@@ -3,12 +3,21 @@
   */
 trait Challenge extends App {
 
-  override def main(args: Array[String]) =
+  val runtime = Runtime.getRuntime
+
+  override def main(args: Array[String]) = {
+    val startedTime = System.currentTimeMillis()
+    val startedMemory = runtime.freeMemory()
     if (args.length > 0)
       super.main(args)
     else {
       val clazz = getClass.getSimpleName.init
       super.main(Array(getClass.getResource(clazz).getPath))
     }
+    val finishedAt = System.currentTimeMillis()
+    val finishedMemory = runtime.freeMemory()
+    println("Execution time: %s ms.".format(finishedAt - startedTime))
+    println("Memory consumed: %skB.".format((startedMemory - finishedMemory) / 1024))
+  }
 
 }
